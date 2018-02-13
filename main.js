1e5c2820069aa501f1b77b53c22ff2b7d9622364
Jake Berg <jacobvictorberg@gmail.com>
2018-02-13 09:29:11 -0500

    added numbers

--- main.js
+++ main.js
@@ -5,111 +5,131 @@ const bestThing = "The best thing about a boolean is even if you are wrong you a
 // Creates a List
 
 const destination = document.querySelector("main");
-const list = document.createElement("ol")
-destination.appendChild(list);
 
 function printResult(result) {
-    var newElement = document.createElement("li");
+    let newElement = document.createElement("div");
     newElement.textContent = JSON.stringify(result);
-    list.appendChild(newElement);
+    destination.appendChild(newElement);
 }
-
 
 // This gets the string bestThing ready by taking out the spaces.
 const noSpace = bestThing.replace(/ /g, "");
+const bestThingArray = bestThing.split(" ")
+
+// This creates an array out of the gotCitiesCSV
+const splitGot = gotCitiesCSV.split(",")
 
 
 // 1. Display an array from the cities in gotCitiesCSV
 
-printResult(gotCitiesCSV);
+printResult("1. " + gotCitiesCSV);
 
 // 2. Display an array of words from the sentence in bestThing
 
-printResult(bestThing.split(" "));
+printResult("2. " + bestThing.split(" "));
 
 // 3. Display a string separated by semi-colons instead of commas from gotCitiesCSV
 
-printResult(gotCitiesCSV.replace(/,/g, ";"));
+printResult("3. " + gotCitiesCSV.replace(/,/g, ";"));
 
 // 4. Display a CSV (comma-separated) string from the lotrCitiesArray
 
-printResult(lotrCitiesArray.join(","));
+printResult("4. " + lotrCitiesArray.join(","));
 
 // 5. Display the first 5 cities in lotrCitiesArray
 
-printResult(lotrCitiesArray.slice(0, 5));
+printResult("5. " + lotrCitiesArray.slice(0, 5));
 
 // 6. Display the last 5 cities in lotrCitiesArray
 
-printResult(lotrCitiesArray.slice(-5));
+printResult("6. " + lotrCitiesArray.slice(-5));
 
 // 7. Display the 3rd to 5th city in lotrCitiesArray
 
-printResult(lotrCitiesArray.slice(3, 6));
+printResult("7. " + lotrCitiesArray.slice(3, 6));
 // 8. Using splice, remove "Rohan" from lotrCitiesArray
 
 lotrCitiesArray.splice(2, 1)
-printResult(lotrCitiesArray);
+printResult("8. " + lotrCitiesArray);
 
 // 9. Using splice, remove all cities after "Dead Marshes" in lotrCitiesArray
 
 lotrCitiesArray.splice(5, 2)
-printResult(lotrCitiesArray);
+printResult("9. " + lotrCitiesArray);
 
 // 10. Using splice, add "Rohan" back to lotrCitiesArray right after "Gondor"
 
 lotrCitiesArray.splice(2, 0, 'Rohan');
-printResult(lotrCitiesArray);
+printResult("10. " + lotrCitiesArray);
 
 // 11. Using splice, rename "Dead Marshes" to "Deadest Marshes" in lotrCitiesArray
 
 lotrCitiesArray.splice(5, 1, 'Deadest Marshes');
-printResult(lotrCitiesArray);
+printResult("11. " + lotrCitiesArray);
 
 
 // 12. Using slice, display the first 14 characters from bestThing
 
-printResult(noSpace.slice(0, 14));
+printResult("12. " + noSpace.slice(0, 14));
 
 // 13. Using slice, display the last 12 characters from bestThing
 
-printResult(noSpace.slice(-14));
+printResult("13. " + noSpace.slice(-14));
 
 // 14. Using slice, display characters between the 23rd and 38th position of bestThing (i.e., "boolean is even"
 
-printResult(noSpace.slice(23, 39));
+printResult("14. " + noSpace.slice(23, 39));
 
 // 15. Repeat #13 using substring instead of slice.
 
-printResult(noSpace.substring(noSpace.length - 14));
+printResult("15. " + noSpace.substring(noSpace.length - 14));
 
 // 16. Repeat #14 using substr instead of slice.
 
 
-printResult(noSpace.substring(23, 39));
+printResult("16. " + noSpace.substring(23, 39));
 
 // 17. Find and display the index of "only" in bestThing
 
-printResult(bestThing.indexOf("only"));
+printResult("17. " + bestThing.indexOf("only"));
 
 // 18. Find and display the index of the last word in bestThing
 
-printResult(bestThing.lastIndexOf("bit"));
+printResult("18. " + bestThing.lastIndexOf("bit"));
 
 // 19. Find and display all cities from gotCitiesCSV  that use double vowels ("aa","ee", etc.)
 
-function dubVowels(){
-
+for (let i = 0; i < splitGot.length; i++) {
+    if (splitGot[i].includes("aa") || splitGot[i].includes("ee") || splitGot[i].includes("ii") || splitGot[i].includes("oo") || splitGot[i].includes("uu")){
+       printResult("19. " + splitGot[i]);
+    }
 }
-
-// var newCities = vowels();
-// printResult(newCities);
 
 // 20. Find and display all cities from lotrCitiesArray that end with "or"
 
+for (let i = 0; i < lotrCitiesArray.length; i++) {
+    if (lotrCitiesArray[i].substring(lotrCitiesArray[i].length - 2) === "or"){
+       
+        printResult("20. " + lotrCitiesArray[i]);
+    }
+}
+
+
 // 21. Find and display all the words in bestThing that start with a "b"
 
+for (let i = 0; i < bestThingArray.length; i++) {
+    if (bestThingArray[i].substring(0, 1) === "b"){
+        printResult("21. " + bestThingArray[i]);
+    }
+}
+
 // 22. Display "Yes" or "No" if lotrCitiesArray includes "Mirkwood"
+
+for (let i = 0; i < lotrCitiesArray.length; i++) {
+    if (lotrCitiesArray[i] === "Mirkwood") {
+        printResult("22. " + "Yes");
+    }
+}
 
 // 23. Display "Yes" or "No" if lotrCitiesArray includes "Hollywood"
 
